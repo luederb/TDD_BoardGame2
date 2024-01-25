72c68dd6607ae952e9ea5420a0f0a1e240c2dfe0
Lueder Budde <lueder.budde@gmail.com>
2024-01-25 11:49:42 +0100

    create second Test for getY and create the method getY

--- src/main/java/org/example/PlayerCharacter.java
+++ src/main/java/org/example/PlayerCharacter.java
@@ -4,4 +4,7 @@ public class PlayerCharacter {
     public static int getX() {
         return 0;
     }
+    public static int getY(){
+        return 0;
+    }
 }
--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -6,11 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerCharacterTest {
     @Test
-    public void getX_whenInputField0_thenReturn0(){
+    public void getX_whenInputX_thenReturn0() {
         //GIVEN:
         int expected = 0;
         //WHEN:
         int actual = PlayerCharacter.getX();
+        //THEN:
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void getY_whenInputY_thenReturn0() {
+        //GIVEN:
+        int expected = 0;
+        //WHEN:
+        int actual = PlayerCharacter.getY();
         //THEN:
         assertEquals(expected, actual);
     }
